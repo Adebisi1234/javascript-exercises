59f8b9211c0763b43364349c05b61fb2f2275eb5
Tobiloba Adebisi <oluwatobilobaadebisi6@gmail.com>
2022-08-12 10:48:08 +0000

    Completed no7 tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(num) {
+// subtract 32 and multiply by . 5556
+  num = (num-32)*0.5556
+  return parseFloat(num.toFixed(1))
 };
 
-const ctof = function() {
-
+const ctof = function(num) {
+// multiply by 1.8 (or 9/5) and add 32.
+  num = (num*1.8)+32
+  return parseFloat(num.toFixed(1))
 };
 
 // Do not edit below this line
