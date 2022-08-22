3f0bd20229abe617cd8379135ed39e930dcc4c67
Tobiloba Adebisi <oluwatobilobaadebisi6@gmail.com>
2022-08-22 15:38:29 +0000

    Solve palindromes question

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    str = str.toLowerCase()
+    let str1 = str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/gi,"")
+    let str2 = str1.replace(/\s/g,"");
 
+    
+    let str3 = str2.split('').reverse().join('')
+    if(str3 === str2){
+        return true;
+    }else{
+        return false;
+    }
 };
 
 // Do not edit below this line
