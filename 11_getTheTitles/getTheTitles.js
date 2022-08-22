e02221ce9b0d614c478196e7c831547aa29cbf64
Tobiloba Adebisi <oluwatobilobaadebisi6@gmail.com>
2022-08-22 16:00:05 +0000

    Solve getTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+    let obj1 =  arr[0].title
+    let obj2 = arr[1].title
+    let newarr = [obj1,obj2]
+    return newarr
+    
 };
 
 // Do not edit below this line
