9b5de54fc0b970c04a6d4d4afb39476ff67a9fdf
Tobiloba Adebisi <oluwatobilobaadebisi6@gmail.com>
2022-08-22 15:48:27 +0000

    Solve fibonacci question

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
-
-};
+const fibonacci = function(num) {
+    if(Math.sign(num) === -1){
+        return 'OOPS'
+    }else{
+        let n1 = 0
+        let n2 = 1
+        let next;
+        for(let i = 0; i < num; i++){
+        next = n1 + n2
+        n1 = n2
+        n2 = next
+        }
+        return n1
+    };
+}
+    
 
 // Do not edit below this line
 module.exports = fibonacci;
