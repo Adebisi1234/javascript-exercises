5df8423fbd491db750eeff30549edb0703899532
Tobiloba Adebisi <oluwatobilobaadebisi6@gmail.com>
2022-08-23 10:10:52 +0000

    Solve getTheTitles in accordance to the solution

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,8 +1,5 @@
 const getTheTitles = function(arr) {
-    let obj1 =  arr[0].title
-    let obj2 = arr[1].title
-    let newarr = [obj1,obj2]
-    return newarr
+    return arr.map(book => {return book.title})
     
 };
 
