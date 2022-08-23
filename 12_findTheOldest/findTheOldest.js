edba3bae6127932c7834e6b5ae3609473f454af3
Tobiloba Adebisi <oluwatobilobaadebisi6@gmail.com>
2022-08-23 10:33:26 +0000

    Solve findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,34 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce((oldestPerson, currentPerson) => {
+        const oldestAge = getAge(oldestPerson.yearOfBirth, oldestPerson.yearOfDeath)
+        const currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath)
+        return oldestAge < currentAge ? currentPerson : oldestPerson
+    })
 
+    function getAge(birth, death){
+        if(!death){
+            death = new Date().getFullYear()
+        }
+        return death - birth
+    }
 };
 
 // Do not edit below this line
 module.exports = findTheOldest;
+// const people = [
+//     {
+//       name: "Carly",
+//       yearOfBirth: 1942,
+//       yearOfDeath: 1970,
+//     },
+//     {
+//       name: "Ray",
+//       yearOfBirth: 1962,
+//       yearOfDeath: 2011,
+//     },
+//     {
+//       name: "Jane",
+//       yearOfBirth: 1912,
+//       yearOfDeath: 1941,
+//     },
+// ]
